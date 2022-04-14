5523c90344115c53b88a2feff91edcd2aee0aaf7
Zeelvashi <103023225+Zeelvashi@users.noreply.github.com>
2022-04-14 15:07:33 +0530

    Update StaffLogin.js

--- src/component/Staff/StaffLogin.js
+++ src/component/Staff/StaffLogin.js
@@ -19,7 +19,7 @@ const StaffLogin = () => {
 
     const login = (e) => {
         e.preventDefault();
-        Axios.post(`https://node-knz.herokuapp.com/stafflogin`, { username, password })
+        Axios.post(`${process.env.LIVE_NODE}/stafflogin`, { username, password })
             .then((res) => {
                 if(res.status === 200) {
                     navigate('/staffhome');
