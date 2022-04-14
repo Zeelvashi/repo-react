33a2c3145dbd8ac28d7e553723b5d7f14bd3f594
Zeelvashi <zeelvashi02@gmail.com>
2022-04-14 16:54:46 +0530

    original

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import './App.css';
 // import Dashboard from "./component/admindash"
 import Header from './component/Header';
-import dotenv, { config } from 'dotenv'
-dotenv.config();
+
+
 function App() {
   return (
     <div className="App">
